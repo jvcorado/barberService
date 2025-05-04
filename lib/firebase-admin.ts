2beac394c3f0a191c0e081e0de6187e2adb4b62f
jvcorado <jhonisilva545@gmail.com>
2025-05-04 14:05:48 -0300

    base64 env

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,15 +1,23 @@
-// lib/firebase-admin.ts
 import { getApps, initializeApp, cert } from "firebase-admin/app";
 import { getStorage } from "firebase-admin/storage";
-import json from "../lib/reserva.json";
+
+if (!process.env.FIREBASE_SERVICE_ACCOUNT_KEY_BASE64) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT_KEY_BASE64 is not defined");
+}
+
+const serviceAccountJson = Buffer.from(
+  process.env.FIREBASE_SERVICE_ACCOUNT_KEY_BASE64,
+  "base64",
+).toString("utf-8");
+
+const serviceAccount = JSON.parse(serviceAccountJson);
 
 if (!getApps().length) {
   initializeApp({
-    credential: cert(json as any),
+    credential: cert(serviceAccount),
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   });
 }
 
 const bucket = getStorage().bucket();
-
 export { bucket };
